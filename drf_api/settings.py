37b32e7fee67c437b68a426aca70d6f3c46a9aa2
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-02-16 18:06:19 +0000

    Completing authentication, authorisation and serializers

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -34,9 +34,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&^*edd*l-3+ta5^bq*@j+nq9ee(lf!%lt32rrhx2o-8f=9g+5x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    os.environ.get("ALLOWED_HOST"),
+    os.environ.get("ALLOWED_HOST_CUSTOM_DOMAIN"),
+    ]
 
 
 # Application definition
